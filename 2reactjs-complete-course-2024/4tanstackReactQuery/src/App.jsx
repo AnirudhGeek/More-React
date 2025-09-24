96f488e1329b84a88c53b4e8c9a3725504be2c06
Anirudh Raturi <annigeek@gmail.com>
2025-09-24 23:19:19 +0530

    gcTime staleTime polling/refectingInterval

--- 2reactjs-complete-course-2024/4tanstackReactQuery/src/App.jsx
+++ 2reactjs-complete-course-2024/4tanstackReactQuery/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./Pages/Home";
 import FetchOld from "./Pages/FetchOld";
 import FetchRQ from "./Pages/FetchRQ";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
 //create a router
 const router = createBrowserRouter([
@@ -35,6 +36,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>;
+      <ReactQueryDevtools initialIsOpen={false}/>
     </QueryClientProvider>
   );
 }; 
--- 2reactjs-complete-course-2024/4tanstackReactQuery/src/Pages/FetchRQ.jsx
+++ 2reactjs-complete-course-2024/4tanstackReactQuery/src/Pages/FetchRQ.jsx
@@ -11,6 +11,10 @@ const FetchRQ = () => {
   } = useQuery({
     queryKey: ["posts"], //think this as useState
     queryFn: fetchPosts, //think this as useEffect
+    // gcTime:10 ,   // cache data will be deleted after every 10milliseconds
+    // staleTime : 5000    // after every 5 seconds data will become stale data
+    refetchInterval : 1000,  //sends request to the server every 1 second to refetch the data
+    refetchIntervalInBackground : true // if its true then we if are doing something in other tabs then fetching will not stop and the request still continues to go into the server
   });
 
   //conditional rendering based on loading, error , and posts data
