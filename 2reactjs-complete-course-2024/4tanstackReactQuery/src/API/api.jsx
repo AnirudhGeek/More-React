93ab6b37242d2d8cc142f30ae624278b6a81edb7
Anirudh Raturi <annigeek@gmail.com>
2025-09-29 23:46:53 +0530

    getting started with infinite scroll

--- 2reactjs-complete-course-2024/4tanstackReactQuery/src/API/api.jsx
+++ 2reactjs-complete-course-2024/4tanstackReactQuery/src/API/api.jsx
@@ -2,30 +2,46 @@ import axios from "axios";
 
 export default async function fetchPosts(pageNumber) {
   try {
-    const res = await axios.get(`https://dummyjson.com/posts?limit=3&skip=${pageNumber}`);
+    const res = await axios.get(
+      `https://dummyjson.com/posts?limit=3&skip=${pageNumber}`
+    );
     return res.status === 200 ? res.data.posts : [];
   } catch (error) {
     console.log(error);
   }
 }
 
-
 //to fetch the individual data
-export const fetchInvPost =async (id) =>{
+export const fetchInvPost = async (id) => {
   try {
-    const res = await axios.get(`https://dummyjson.com/posts/${id}`)
-    return res.status ===200 ? res.data : [ ]
+    const res = await axios.get(`https://dummyjson.com/posts/${id}`);
+    return res.status === 200 ? res.data : [];
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-//to delete post 
-export const deletePost = async(id)=>{
-  return await axios.delete(`https://dummyjson.com/posts/${id}`)
-}
+//to delete post
+export const deletePost = async (id) => {
+  return await axios.delete(`https://dummyjson.com/posts/${id}`);
+};
 
 //to update the post
-export const updatePost = async(id)=>{
-  return await axios.patch(`https://dummyjson.com/posts/${id}`,{title : "I have updated"})
-}
+export const updatePost = async (id) => {
+  return await axios.patch(`https://dummyjson.com/posts/${id}`, {
+    title: "I have updated",
+  });
+};
+
+//infinite scrolling
+export const fetchUsers = async ({ pageParam = 1 }) => {
+  try {
+    const res = await axios.get(
+      `https://api.github.com/users?per_page=10&page=${pageParam}`
+    );
+
+    return res.data
+  } catch (error) {
+    console.log(error);
+  }
+};
