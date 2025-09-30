6525ec048e0295e2e38f4fcd39cc2b885b8b559c
Anirudh Raturi <annigeek@gmail.com>
2025-09-30 22:28:39 +0530

    infinite scrolling using tanstack

--- 2reactjs-complete-course-2024/4tanstackReactQuery/src/App.jsx
+++ 2reactjs-complete-course-2024/4tanstackReactQuery/src/App.jsx
@@ -7,6 +7,7 @@ import FetchRQ from "./Pages/FetchRQ";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import FetchInd from "./components/UI/FetchInd";
+import InfiniteScroll from "./Pages/InfiniteScroll";
 
 //create a router
 const router = createBrowserRouter([
@@ -29,7 +30,11 @@ const router = createBrowserRouter([
       {
         path :"rq/:id",
         element : <FetchInd/>
-      }
+      },
+      {
+        path :"/infinite",
+        element : <InfiniteScroll/>
+      },
     ],
   },
 ]);
---
+++ 2reactjs-complete-course-2024/4tanstackReactQuery/src/Pages/InfiniteScroll.jsx
@@ -0,0 +1,60 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { fetchUsers } from "../API/api";
+
+const InfiniteScroll = () => {
+  const { data, hasNextPage, fetchNextPage , status ,isFetchingNextPage} = useInfiniteQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+    getNextPageParam: (lastPage, allPages) => {
+      //the use of getNextPageParamis to check whether there is any next page or not
+      console.log("lastpage", lastPage, allPages); //basically the use of the last page is the content we are seeing and allPages is number of pages rendered
+      return lastPage.length === 10 ? allPages.length + 1 : undefined;
+    },
+  });
+
+  console.log(data);
+
+  const handleScroll = ()=>{
+    const bottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1
+
+    if(bottom && hasNextPage){
+      fetchNextPage()
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+    return()=>window.removeEventListener('scroll',handleScroll)
+  },[hasNextPage])
+
+  if(status === "loading") return <div>Loading...</div>
+  if(status === "error") return <div>Error fetching data</div>
+
+  return (
+    <div>
+      <h1>Infinite Scroll with React Query v5</h1>
+
+      {data?.pages?.map((page, index) => (
+        <ul key={index}>
+          {page.map((user) => (
+            <li key={user.id} className="p-2 border-[#ccc]">
+              <p>{user.login}</p>
+              <img
+                src={user.avatar_url}
+                alt={user.login}
+                width={50}
+                height={50}
+              />
+            </li>
+          ))}
+        </ul>
+      ))}
+
+      {/* if we want loading state while its fetching the component */}
+      {isFetchingNextPage && <div>Loading more...</div>}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
